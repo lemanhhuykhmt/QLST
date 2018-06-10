9eb4876effa689b84d518a637b85736e2d4e8b04
leson11051997 <40015063+leson11051997@users.noreply.github.com>
2018-06-10 23:47:09 +0700

    Add Class User
    Add Class User

---
+++ QLST/Model/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLST.Model
+{
+    public class Users
+    {
+        public string  UserName { get; set; }
+        public string PassWord { get; set; }
+
+    }
+}
